2e048977b1bf71cb12e19dd24dc0f32eda0a34c1
evgeny <anonimno@gmail.com>
2023-01-29 20:37:31 +1100

    Реализовал экран настроек Вынес размеры в константы

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,20 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+
+        findViewById<ImageView>(R.id.back_to_main_activity).setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
