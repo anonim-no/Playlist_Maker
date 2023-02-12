82bf46141df876513dcdb079c9a6230302c0fe0d
evgeny <anonimno@gmail.com>
2023-02-12 17:09:39 +1100

    - Реализованы кнопки «Поделиться приложением», «Написать разработчикам», «Пользовательское соглашение»

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,12 @@
 package com.example.playlistmaker
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -14,6 +17,31 @@ class SettingsActivity : AppCompatActivity() {
         // закрываем SettingsActivity и возвращаемся на предыдущее
         findViewById<ImageView>(R.id.back_to_main_activity).setOnClickListener {
             finish()
+        }
+
+        // Поделиться - текст-ссылка
+        findViewById<Button>(R.id.button_sharing).setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_share_link))
+            intent.type = "text/plain"
+            startActivity(intent)
+        }
+
+        // Обратиться в техподдержку - отправляем в почту
+        findViewById<Button>(R.id.button_support).setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_email_text))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_email_subject))
+            startActivity(intent)
+        }
+
+        // Пользовательское соглашение - отправляем в браузер
+        findViewById<Button>(R.id.button_terms).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(getString(R.string.support_terms_link))
+            startActivity(intent)
         }
 
 
