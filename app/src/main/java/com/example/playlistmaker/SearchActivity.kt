ee88af35a65306f81a3bbc2c572826af1a7e4fb2
evgeny <anonimno@gmail.com>
2023-02-12 19:10:04 +1100

    - Верстка экрана поиска

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,10 +2,17 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        // закрываем SearchActivity и возвращаемся на предыдущее
+        findViewById<ImageView>(R.id.back_to_main_activity).setOnClickListener {
+            finish()
+        }
+
     }
 }
