5c63ffa7da30d3ca36f9de0dfd4846ca90412ae9
evgeny <anonimno@gmail.com>
2023-02-13 21:44:21 +1100

    - В настройках убран createChooser, startActivity обернут в try/catch с сообщение об ошибке в "тосте"

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.playlistmaker
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -24,7 +27,15 @@ class SettingsActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_SEND)
             intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_share_link))
             intent.type = "text/plain"
-            startActivity(Intent.createChooser(intent, null))
+            try {
+                startActivity(intent)
+            } catch (ex: ActivityNotFoundException) {
+                Toast.makeText(
+                    this@SettingsActivity,
+                    getString(R.string.settings_not_found_app),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
 
         // Обратиться в техподдержку - отправляем в почту
@@ -34,14 +45,30 @@ class SettingsActivity : AppCompatActivity() {
             intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
             intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_email_text))
             intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_email_subject))
-            startActivity(Intent.createChooser(intent, null))
+            try {
+                startActivity(intent)
+            } catch (ex: ActivityNotFoundException) {
+                Toast.makeText(
+                    this@SettingsActivity,
+                    getString(R.string.settings_not_found_app),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
 
         // Пользовательское соглашение - отправляем в браузер
         findViewById<Button>(R.id.button_terms).setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(getString(R.string.support_terms_link))
-            startActivity(Intent.createChooser(intent, null))
+            try {
+                startActivity(intent)
+            } catch (ex: ActivityNotFoundException) {
+                Toast.makeText(
+                    this@SettingsActivity,
+                    getString(R.string.settings_not_found_app),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
 
 
