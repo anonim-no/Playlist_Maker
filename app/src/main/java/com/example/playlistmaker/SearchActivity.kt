af9d150fa7104df43b2faa53f9d5042091e2687c
anonimNO <anonimno@gmail.com>
2023-02-15 10:55:58 +1100

    - Рефакторинг кода SearchActivity

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -16,54 +16,54 @@ class SearchActivity : AppCompatActivity() {
     }
 
     // в переменную сохраняется текст из поисковой строки при изменении
-    // можно обойтись и без этой переменной, но в ТЗ она требуется
-    private var searchQwery = ""
+    private var searchInputQwery = ""
+
+    private lateinit var searchInput: EditText
+    private lateinit var searchInputClearButton: ImageView
+    private val searchInputTextWatcher = object : TextWatcher {
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            // при изменении текста скрываем или показываем кнопку очистки формы
+            searchInputClearButton.visibility = clearButtonVisibility(s)
+            // сохраняем тест в переменную
+            searchInputQwery = s.toString()
+        }
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun afterTextChanged(s: Editable?) {}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-
-        val clearButton = findViewById<ImageView>(R.id.clear_search_form)
-        val editText = findViewById<EditText>(R.id.input_search_form)
 
         // закрываем SearchActivity и возвращаемся на предыдущее
         findViewById<ImageView>(R.id.back_to_main_activity).setOnClickListener {
             finish()
         }
 
+        searchInput = findViewById(R.id.input_search_form)
+        // ставим фокус на форму поиска
+        searchInput.requestFocus()
+        // к форме поиска добавляем обработчик ввода текста
+        searchInput.addTextChangedListener(searchInputTextWatcher)
+
+        searchInputClearButton = findViewById(R.id.clear_search_form)
+        // при запуске скрываем или показываем кнопку очистки формы
+        searchInputClearButton.visibility = clearButtonVisibility(searchInput.text)
         // очищаем форму поиска
-        clearButton.setOnClickListener {
-            editText.setText("")
-
-            // Прячем клавиатуру, это есть в ТЗ
-            // У кнопки «Очистить» есть OnClickListener, в котором очищается поисковый запрос и прячется клавиатура
-            val view = this.currentFocus
-            if (view != null) {
-                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(view.windowToken, 0)
-            }
+        searchInputClearButton.setOnClickListener {
+            clearSearchForm()
         }
+    }
 
-        // при запуске скрываем или показываем кнопку очистки формы и устанавливаем фокус в поле поиска
-        clearButton.visibility = clearButtonVisibility(editText.text)
-        editText.requestFocus()
+    private fun clearSearchForm() {
+        searchInput.setText("")
 
-        val simpleTextWatcher = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // empty
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // при изменении текста скрываем или показываем кнопку очистки формы
-                clearButton.visibility = clearButtonVisibility(s)
-                // сохраняем тест в переменную
-                searchQwery = s.toString()
-            }
-            override fun afterTextChanged(s: Editable?) {
-                // empty
-            }
+        // Прячем клавиатуру
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
-        editText.addTextChangedListener(simpleTextWatcher)
-
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -77,18 +77,14 @@ class SearchActivity : AppCompatActivity() {
     // перед уничтожением активити сохраняем всё что введено в поле поискового запроса
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(SEARCH_QUERY, searchQwery)
+        outState.putString(SEARCH_QUERY, searchInputQwery)
     }
 
-    // Можно обойтись без onRestoreInstanceState, потому что в методе onCreate тоже передаётся Bundle
-    // Но в ТЗ указано:
-    // "Переопределён метод onRestoreInstantState().
-    // В этом методе в переменную для хранения текста поискового запроса помещается сохранённый текст.
-    // Восстановленный текст помещается в EditText."
+    // восстанавливаем текст и помещаем в EditText
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        searchQwery = savedInstanceState.getString(SEARCH_QUERY,"")
-        findViewById<EditText>(R.id.input_search_form).setText(searchQwery)
+        searchInputQwery = savedInstanceState.getString(SEARCH_QUERY,"")
+        searchInput.setText(searchInputQwery)
     }
 
 }
