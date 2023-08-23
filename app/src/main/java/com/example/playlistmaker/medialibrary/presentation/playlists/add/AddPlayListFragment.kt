fb837ee4df1b82cce5ddf83b08113f4e1b12c614
Evgeniy Shurygin <anonimno@gmail.com>
2023-08-23 17:11:28 +1100

    - добавлен диалог при выходе, при несохраненных данных - добавлена активация кнопки создания, при заполнении полей - добавлен выбор картинки

--- app/src/main/java/com/example/playlistmaker/medialibrary/presentation/playlists/add/AddPlayListFragment.kt
+++ app/src/main/java/com/example/playlistmaker/medialibrary/presentation/playlists/add/AddPlayListFragment.kt
@@ -1,12 +1,19 @@
 package com.example.playlistmaker.medialibrary.presentation.playlists.add
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentAddplaylistBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AddPlayListFragment : Fragment() {
@@ -14,6 +21,10 @@ class AddPlayListFragment : Fragment() {
     private lateinit var binding: FragmentAddplaylistBinding
 
     private val addPlayListViewModel: AddPlayListViewModel by viewModel()
+
+    private lateinit var confirmDialog: MaterialAlertDialogBuilder
+
+    private var pickImage = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,11 +37,65 @@ class AddPlayListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.toolbar.setOnClickListener {
-            // TODO: проверка на несохраненные данные и подверждение выхода
-            findNavController().popBackStack()
+        confirmDialog = MaterialAlertDialogBuilder(requireContext()).apply {
+            setTitle(getString(R.string.finish_creating_playlist))
+            setMessage(getString(R.string.all_unsaved_data_will_be_lost))
+            setNegativeButton(getString(R.string.cancel)) { dialog, which ->
+            }
+            setPositiveButton(getString(R.string.finish)) { dialog, which ->
+                findNavController().navigateUp()
+            }
         }
 
+        binding.toolbar.setOnClickListener {
+            if (checkUnsavedData()) {
+                confirmDialog.show()
+            } else {
+                findNavController().popBackStack()
+            }
+        }
+
+        binding.playListNameEditText.doOnTextChanged { s: CharSequence?, _, _, _ ->
+            binding.playListCreateButton.isEnabled = !s.isNullOrEmpty()
+        }
+
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                if (uri != null) {
+                    binding.addImage.setImageURI(uri)
+                    pickImage = true
+                }
+            }
+        binding.addImage.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                myHandleOnBackPressed()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    private fun myHandleOnBackPressed() {
+        if (checkUnsavedData()) {
+            confirmDialog.show()
+        } else {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun checkUnsavedData(): Boolean {
+        return (
+                pickImage
+                || binding.playListNameEditText.text.toString().isNotEmpty()
+                || binding.playListDescriptionEditText.text.toString().isNotEmpty()
+                )
     }
 
     companion object {
