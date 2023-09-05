7e2a603c4955e0f6b54a5430f2ef391e43d18da5
Evgeniy Shurygin <anonimno@gmail.com>
2023-09-05 17:26:44 +1100

    - добавлен экран плейлиста, начало вертски

---
+++ app/src/main/java/com/example/playlistmaker/medialibrary/presentation/playlists/PlayListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.playlistmaker.medialibrary.presentation.playlists
+
+import androidx.lifecycle.ViewModel
+
+class PlayListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
